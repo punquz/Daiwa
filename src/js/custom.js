6d34bcbaa339571d0bd466a1d4cadf57522e1cdf
punkuz shah <biker_boyzpunk@hotmail.com>
2018-08-19 22:52:37 +0545

    daiwa project

---
+++ src/js/custom.js
@@ -0,0 +1,5 @@
+// to top scroll
+$("#to-top").click(function() {
+     $("html, body").animate({ scrollTop: 0 }, 1200);
+     return false;
+});
---
+++ src/js/script.js
@@ -0,0 +1,9 @@
+// smooth scrolling
+$('a[href^="#"]').click(function () {
+    $('html, body').animate({
+        scrollTop: $('[name="' + $.attr(this, 'href').substr(1) + '"]').offset().top
+    }, 1000);
+
+    return false;
+});
+
